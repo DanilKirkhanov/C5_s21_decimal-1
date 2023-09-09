bc2746b84947a38394a16e611e6775fff13e772f
Fawn Synch <fawnsync@un-n6.kzn.21-school.ru>
2023-09-09 22:58:48 +0300

    hvatit :(

--- src/s21_comparison_operators.c
+++ src/s21_comparison_operators.c
@@ -2,42 +2,39 @@
 
 int s21_is_equal(s21_decimal dec_1, s21_decimal dec_2) {
   int result = 1;
-  // s21_decimal zero = {{0, 0, 0, 0}};
-  if ((dec_1.bits[0] | dec_1.bits[1] | dec_1.bits[2] | dec_2.bits[0] |
-       dec_2.bits[1] | dec_2.bits[2]) != 0) {
-    if (s21_getScale(dec_1) != s21_getScale(dec_2))
-      s21_scaleEqul(&dec_1, &dec_2);
-    if (dec_1.bits[0] != dec_2.bits[0]) {
-      result = 0;
-    } else if (dec_1.bits[1] != dec_2.bits[1]) {
-      result = 0;
-    } else if (dec_1.bits[2] != dec_2.bits[2]) {
-      result = 0;
-    } else if (s21_getSign(dec_1) != s21_getSign(dec_2)) {
-      result = 0;
-    } else {
-      result = 1;
-    }
+  if (s21_getScale(dec_1) != s21_getScale(dec_2))
+    s21_scaleEqul(&dec_1, &dec_2);
+  s21_decimal zero = {{0, 0, 0, 0}};
+  if (s21_equalWithoutSign(zero, dec_1) == 1 &&
+      s21_equalWithoutSign(zero, dec_2) == 1) {
+    result = 1;
+  } else if (s21_getSign(dec_1) != s21_getSign(dec_2)) {
+    result = 0;
+  } else if (dec_1.bits[2] != dec_2.bits[2]) {
+    result = 0;
+  } else if (dec_1.bits[1] != dec_2.bits[1]) {
+    result = 0;
+  } else if (dec_1.bits[0] != dec_2.bits[0]) {
+    result = 0;
+  } else {
+    result = 1;
   }
   return result;
 }
 
 int s21_is_less(s21_decimal dec_1, s21_decimal dec_2) {
-  int result = 0;
-  //   if ((dec_1.bits[0] | dec_1.bits[1] | dec_1.bits[2] | dec_2.bits[0] |
-  //        dec_2.bits[1] | dec_2.bits[2]) != 0) {
+  int result = 1;
   s21_decimal zero = {{0, 0, 0, 0}};
   int sign_1 = s21_getSign(dec_1);
   int sign_2 = s21_getSign(dec_2);
-  if (s21_getScale(dec_1) != s21_getScale(dec_2)) {
+  if (s21_getScale(dec_1) != s21_getScale(dec_2))
     s21_scaleEqul(&dec_1, &dec_2);
-  }
   if (s21_equalWithoutSign(dec_1, zero) == 1 &&
       s21_equalWithoutSign(dec_2, zero) == 1) {
     result = 0;
-  } else if (&dec_1 < &dec_2) {
+  } else if (sign_1 < sign_2) {
     result = 0;
-  } else if (&dec_1 > &dec_2) {
+  } else if (sign_1 > sign_2) {
     result = 1;
   } else {
     result = s21_less_without_mod(dec_1, dec_2);
@@ -47,7 +44,6 @@ int s21_is_less(s21_decimal dec_1, s21_decimal dec_2) {
       result = 1;
     }
   }
-  //  }
   return result;
 }
 
--- src/s21_other.c
+++ src/s21_other.c
@@ -45,3 +45,31 @@ void s21_add_1_(s21_decimal* result) {
     }
   }
 }
+
+
+int s21_round(s21_decimal value, s21_decimal* result) {
+  int err = 0;
+  if (!result)
+    err = 1;
+  else {
+    int sign = s21_get_sign(value);
+    value.bits[3] = s21_set_bit(value.bits[3], 31);
+    s21_decimal five = {{5, 0, 0, 0x10000}};
+    s21_decimal one = {{1, 0, 0, 0}};
+    s21_decimal trun = {{0, 0, 0, 0}};
+    s21_decimal sub_dec = {{0, 0, 0, 0}};
+
+    s21_truncate(value, &trun);
+    s21_sub(value, trun, &sub_dec);
+    if (s21_is_greater_or_equal(five, sub_dec))
+      s21_add(trun, one, result);
+    else
+      *result = trun;
+    if (sign)
+      result->bits[3] =
+          0x80000000;  // зануляем скейл и ставим отрицательный знак
+    else
+      result->bits[3] = 0;  // зануляем скейл, оставляется положительный знак
+  }
+  return err;
+}
