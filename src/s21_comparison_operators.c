50c2e4e8b1c49b4d69d88b18d68470b0aaf07095
Fawn Synch <fawnsync@un-n6.kzn.21-school.ru>
2023-09-15 16:12:14 +0300

    danya help

--- src/s21_comparison_operators.c
+++ src/s21_comparison_operators.c
@@ -1,46 +1,49 @@
 #include "s21_decimal.h"
 
-int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
-  int sign = s21_getSign(value_1) ^ s21_getSign(value_2);
+int s21_is_equal(s21_decimal dec_1, s21_decimal dec_2) {
+  int sign = s21_getSign(dec_1) ^ s21_getSign(dec_2);
   int equal = 1;
-  if ((value_1.bits[0] | value_1.bits[1] | value_1.bits[2] | value_2.bits[0] |
-       value_2.bits[1] | value_2.bits[2]) != 0) {
-    equal = sign == 0 ? 1 : 0;
+  if ((dec_1.bits[0] | dec_1.bits[1] | dec_1.bits[2] | dec_2.bits[0] |
+       dec_2.bits[1] | dec_2.bits[2]) != 0) {
+    equal = (sign == 0);
     if (equal) {
-      s21_double_decimal double_value_1 = {};
-      s21_double_decimal double_value_2 = {};
-      s21_double_to_decimal(value_1, &double_value_1);
-      s21_double_to_decimal(value_2, &double_value_2);
-      s21_double_common_denominator(&double_value_1, &double_value_2);
-      for (int i = 0; i < 3 && equal == 1; i++) {
-        if ((double_value_1.bits[i] ^ double_value_2.bits[i]) != 0) equal = 0;
+      s21_double_decimal double_dec_1 = {};
+      s21_double_decimal double_dec_2 = {};
+      s21_double_to_decimal(dec_1, &double_dec_1);
+      s21_double_to_decimal(dec_2, &double_dec_2);
+      s21_double_common_denominator(&double_dec_1, &double_dec_2);
+      for (int i = 0; i < 3 && equal; i++) {
+        if ((double_dec_1.bits[i] ^ double_dec_2.bits[i]) != 0)
+          equal = 0;
       }
     }
   }
   return equal;
 }
 
-int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
+int s21_is_less(s21_decimal dec_1, s21_decimal dec_2) {
   int less = 0;
-  if ((value_1.bits[0] | value_1.bits[1] | value_1.bits[2] | value_2.bits[0] |
-       value_2.bits[1] | value_2.bits[2]) != 0) {
-    s21_double_decimal double_value_1 = {};
-    s21_double_decimal double_value_2 = {};
-    s21_double_to_decimal(value_1, &double_value_1);
-    s21_double_to_decimal(value_2, &double_value_2);
-    int sign_1 = s21_double_getSign(double_value_1);
-    int sign_2 = s21_double_getSign(double_value_2);
+  if ((dec_1.bits[0] | dec_1.bits[1] | dec_1.bits[2] | dec_2.bits[0] |
+       dec_2.bits[1] | dec_2.bits[2]) != 0) {
+    s21_double_decimal double_dec_1 = {};
+    s21_double_decimal double_dec_2 = {};
+    s21_double_to_decimal(dec_1, &double_dec_1);
+    s21_double_to_decimal(dec_2, &double_dec_2);
+    int sign_1 = s21_double_getSign(double_dec_1);
+    int sign_2 = s21_double_getSign(double_dec_2);
     if (sign_1 == sign_2) {
-      s21_double_common_denominator(&double_value_1, &double_value_2);
+      s21_double_common_denominator(&double_dec_1, &double_dec_2);
       for (int i = 5; i >= 0 && less == 0; i--) {
-        if (double_value_1.bits[i] > double_value_2.bits[i])
-          less = sign_1 != 1 ? 2 : 1;
-        else if (double_value_1.bits[i] < double_value_2.bits[i])
-          less = sign_1 != 1 ? 1 : 2;
+        if (double_dec_1.bits[i] > double_dec_2.bits[i]) {
+          less = (sign_1 != 1) ? 2 : 1;
+        } else if (double_dec_1.bits[i] < double_dec_2.bits[i]) {
+          less = (sign_1 != 1) ? 1 : 2;
+        }
       }
-      less = less == 2 ? 0 : less;
-    } else if (sign_1 == 1)
+      less = (less == 2) ? 0 : less;
+    } else if (sign_1 == 1) {
       less = 1;
+    }
   }
   return less;
 }
--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct {
   unsigned int bits[4];
